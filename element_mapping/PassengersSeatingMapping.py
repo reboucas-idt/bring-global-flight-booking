aadaa8b1d9b476366fd4703fe78407528914b6ef
Alexandre Rebouças <alexgr@Alexandres-MacBook-Pro.local>
2021-04-03 00:36:54 -0300

    Include functions for Trip search

--- element_mapping/PassengersSeatingMapping.py
+++ element_mapping/PassengersSeatingMapping.py
@@ -6,11 +6,6 @@ BTN_SEAT_FIRST_PASSENGER_DEPARTURE='xpath://html/body/seats-root/div/div/seats-c
 BTN_SEAT_SECOND_PASSENGER_DEPARTURE='xpath://html/body/seats-root/div/div/seats-container-root/seats-container-v2/main/div[2]/div/div/div/div/div/div[1]/seat-map/div/div[19]/div/button[2]'
 BTN_SEAT_THIRD_PASSENGER_DEPARTURE='xpath://html/body/seats-root/div/div/seats-container-root/seats-container-v2/main/div[2]/div/div/div/div/div/div[1]/seat-map/div/div[19]/div/button[3]'
 
-#--- Seat Information - Return ---#
-BTN_SEAT_FIRST_PASSENGER_RETURN=''
-BTN_SEAT_SECOND_PASSENGER_RETURN=''
-BTN_SEAT_THIRD_PASSENGER_RETURN=''
-
 #--- Miscelaneous ---#
 BTN_FAMILY_SEATING_ADVICE='xpath://button[@class="seats-modal__cta ry-button--gradient-blue"]'
 BTN_SEATS_NEXT_FLIGHT='xpath://button[@class="ry-button ry-button--gradient-blue seats-action__button ry-button--flat-blue"]'
